c65e999889a44c5066ea3a9918cf0b9cb831b93c
sydorenkovd <sydorenkovd@gmail.com>
2015-12-23 21:20:28 +0200

    Navigation class

--- classes/Navigation.php
+++ classes/Navigation.php
@@ -1,4 +1,29 @@
 <?php
 class Navigation{
+    public $objUrl;
+    public $classActive = 'act';
 
+    public function __construct($objUrl = null){
+        $this->objUrl = is_object($objUrl) ? $objUrl : new Url();
+    }
+    public function active($main = null, $pairs = null, $single = null){
+        if(!empty($main)){
+            if(empty($pairs)){
+                if($main == $this->objUrl->main){
+                    return !$single ? ' ' . $this->classActive : ' class="' . $this->classActive . '"';
+                }
+            } else {
+                $exceptions = [];
+                foreach($pairs as $key => $value){
+                    $paramsUrl = $this->objUrl->get($key);
+                    if($paramsUrl != $value){
+                        $exceptions[] = $key;
+                    }
+                }
+                if($main == $this->objUrl->main && empty($exceptions)){
+                    return !$single ? ' ' . $this->classActive : ' class="' . $this->classActive . '"';
+                }
+            }
+        }
+    }
 }
